fbbe31296fd1731c1fa4404cc7dc9084ba9e12e2
Ignácio Seco <itenorioseco@gmail.com>
2022-11-29 19:37:52 -0300

    para criar uma estrutura de dados

---
+++ config/db.config.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+
+export async function connect() {
+    try {
+        const dbConnect = await mongoose.connect(process.env.MONGODB_URI)
+
+        console.log(`Conectado ao DB: ${dbConnect.connection.name}`)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import processRouter from './routes/process.routes.js'
+import { connect } from './config/db.config.js'
 
 dotenv.config()
+connect()
 const app = express()
 app.use(express.json())
 app.use('/processos', processRouter)
